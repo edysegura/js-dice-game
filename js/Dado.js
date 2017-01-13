966cded659db90ecc78cf93bcc7291a594f08259
Edy Segura <edysegura@gmail.com>
2017-01-13 20:19:58 -0200

    First commit as baseline

---
+++ js/Dado.js
@@ -0,0 +1,22 @@
+﻿/**
+ *
+ * Objeto Literal Dado
+ * para setar o eventos
+ * @author Edy Segura, edy@segura.pro.br
+ *
+ */
+ 
+var Dado = {
+	
+	lancarDados: function() {
+		/*
+			o método Math.random() gera um número pseudo randomico
+			de 0 até 1. ex: 0.5 ou 0.9 ou 1
+			É multiplicado por 6 porque queremos somente números de
+			1 a 6
+			É somado por 1 porque não queremos o número 0
+		*/
+		return parseInt(1 + Math.random() * 6);
+	}
+	
+};
---
+++ js/Index.js
@@ -0,0 +1,60 @@
+﻿/**
+ *
+ * Objeto Literal Index
+ * @author Edy Segura, edy@segura.pro.br
+ *
+ */
+var Index = {
+
+	images: [],
+	
+	init: function() {
+		Index.preloadImages();
+		Index.setForm();
+	},
+	
+	
+	preloadImages: function() {
+		for(var i=1; i<=6; i++) {
+			var image = new Image();
+			image.src = "images/dado0" + i + ".gif";
+			Index.images.push(image);
+		}
+	},
+	
+	
+	setForm: function() {
+		var form = document.forms[0];
+		if(form) {
+			form.onsubmit = function() {
+				Index.showResult(this);
+				return false;
+			}
+		}
+	},
+	
+	
+	showResult: function(form) {
+		var imgDado1  = document.getElementById('imgD1');
+		var imgDado2  = document.getElementById('imgD2');
+		
+		var valorD1   = Index.lancaDado();
+		var valorD2   = Index.lancaDado();
+		
+		form.d1.value = valorD1;
+		form.d2.value = valorD2;
+		
+		imgDado1.src  = Index.images[valorD1-1].src; //"images/dado0" + valorD1 + ".gif";
+		imgDado2.src  = Index.images[valorD2-1].src; //"images/dado0" + valorD2 + ".gif";
+	},
+	
+	
+	lancaDado: function() {
+	  // é somado + 1 para nunca retornar zero.
+		// e multiplicado por 6 para gerar números randônicos de 1 a 6.
+		return parseInt(1 + Math.random() * 6);
+	}
+}
+
+//inicializacao
+window.onload = Index.init;
